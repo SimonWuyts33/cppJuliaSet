0f17eec5be0b27360ca003414f2c727049b5a4a3
Simon Wuyts <wuyts_simon@hotmail.com>
2018-12-05 12:02:13 +0000

    added openCL helper methods

---
+++ TBB_example02/setup_cl.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#pragma once
+
+#include <CL\opencl.h>
+
+cl_context createContext();
+
+cl_device_id getDeviceForContext(cl_context context, int deviceIndex = 0);
+
+cl_command_queue createCommandQueue(cl_context context, cl_device_id *device);
+
+cl_program createProgram(cl_context context, cl_device_id device, const char* fileName);
